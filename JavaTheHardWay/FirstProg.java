308a7e41b574303518d2a9fc4232c97c290855a7
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-02-28 17:29:29 -0500

    Completed Exercise 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,7 +1,6 @@
 public class FirstProg {
   public static void main(String[] args){
-
-    System.out.println("I am determined to learn how to code.");
-    System.out.println("Today's date is");
+    System.out.println("I, Sergei Samoiloff, am determined to learn how to code.");
+    System.out.println("Today's date is February 28th, 2020.");
   }
 }
