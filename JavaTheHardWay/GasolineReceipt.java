90691c183c66b9e2dfb00e9b46edfb7db570931d
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-02-28 18:33:58 -0500

    Completed Exercise 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main(String[] args) {
+    System.out.println("+------------------------+");
+    System.out.println("|                        |");
+    System.out.println("|      CORNER STORE      |");
+    System.out.println("|                        |");
+    System.out.println("| 2020-02-28  18:31PM    |");
+    System.out.println("|                        |");
+    System.out.println("| Gallons:       10.870  |");
+    System.out.println("| Price/gallon: $ 2.889  |");
+    System.out.println("|                        |");
+    System.out.println("| Fuel total:  $ 31.40   |");
+    System.out.println("|                        |");
+    System.out.println("+------------------------+");
+  }
+}
