a828f7aa067754cb2478ee4aed998cba2fadcceb
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-02-29 00:04:56 -0500

    Completed Objective 2 Lab 4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 7.62;
+    num2 = 5.56;
+    num3 = -0.4;
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println(average);
+  }
+}
