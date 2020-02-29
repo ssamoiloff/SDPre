5aca661e4286f6c223012089cf7d5a93c86c8922
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-02-28 23:19:21 -0500

    Completed Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Sergei Samoiloff";
+    int age = 34;
+    String state = "Oregon";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
