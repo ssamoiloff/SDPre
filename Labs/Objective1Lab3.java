d26fd24b56081de29995db43b2b3a2ac5c4d3faa
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-02-28 20:42:32 -0500

    Completed Lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+
+    System.out.println("Name : Sergei Samoiloff");
+    System.out.println("Age : 34");
+    System.out.println("State : Oregon");
+  }
+}
